18817b83ce7eeabad59fd5dffa50571f52294741
keshe4ka <artem.anaschenko@gmail.com>
2022-03-08 04:15:05 +0300

    Списки словарей перенесены в data.py

---
+++ Flask_shop_api/shop_api/data.py
@@ -0,0 +1,25 @@
+categories = [
+    {
+        'id': 1,
+        'name': 'first_category'
+    },
+    {
+        'id': 2,
+        'name': 'second_category'
+    }
+]
+
+products = [
+    {
+        "id": 1,
+        "name": "string",
+        "description": "string",
+        "category_id": 1
+    },
+    {
+        "id": 2,
+        "name": "string",
+        "description": "desc",
+        "category_id": 1
+    }
+]
--- Flask_shop_api/shop_api/flask_shop_api.py
+++ Flask_shop_api/shop_api/flask_shop_api.py
@@ -1,32 +1,7 @@
 from flask import Flask, jsonify, request
+from data import products, categories
 
 app = Flask(__name__)
-
-categories = [
-    {
-        'id': 1,
-        'name': 'first_category'
-    },
-    {
-        'id': 2,
-        'name': 'second_category'
-    }
-]
-
-products = [
-    {
-        "id": 1,
-        "name": "string",
-        "description": "string",
-        "category_id": 1
-    },
-    {
-        "id": 2,
-        "name": "string",
-        "description": "desc",
-        "category_id": 1
-    }
-]
 
 
 def find_all_by_keys(keys, value):
